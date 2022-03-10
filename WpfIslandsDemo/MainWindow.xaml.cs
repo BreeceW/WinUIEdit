a487de8324dbedcce0b15f8a01db462a88b2fee9
Breece Walker <breece@outlook.com>
2022-03-10 14:55:33 -0500

    Add XAML Islands demo projects

---
+++ WpfIslandsDemo/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIslandsDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfIslandsDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WpfIslandsDemo
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (new IslandsUwpDemoApp.App())
+            {
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
