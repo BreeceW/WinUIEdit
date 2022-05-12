32318071dc2df78a385591853819ebbd628f7b70
Breece Walker <breece@outlook.com>
2022-05-11 23:25:36 -0400

    Add WinForms islands sample

---
+++ WinFormsIslandsDemo/MainForm.cs
@@ -0,0 +1,22 @@
+using Microsoft.Toolkit.Forms.UI.XamlHost;
+using System.Windows.Forms;
+using Windows.UI.Xaml;
+
+namespace WinFormsIslandsDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            var host = new WindowsXamlHost
+            {
+                Dock = DockStyle.Fill,
+            };
+            var control = Microsoft.Toolkit.Win32.UI.XamlHost.UWPTypeFactory.CreateXamlContentByType("MicaEditor.MicaEditorControl");
+            host.Child = control;
+            Controls.Add(host);
+        }
+    }
+}
