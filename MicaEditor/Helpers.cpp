e311fff1dab3abebf889d6dabf1db377972bc0f1
Breece Walker <breece@outlook.com>
2023-04-12 17:29:58 -0400

    Fix issue with UWP certification on Microsoft Store

--- MicaEditor/Helpers.cpp
+++ MicaEditor/Helpers.cpp
@@ -10,10 +10,21 @@ namespace MicaEditor
 		return static_cast<int>(rounded ? floorf(pixelVal + 0.5f) : pixelVal); // Todo: Test if this is ever necessary
 	}
 
+	typedef LONG(*AppPolicyGetWindowingModelPtr)(HANDLE processToken, AppPolicyWindowingModel *policy);
+	static AppPolicyGetWindowingModelPtr s_appPolicyGetWindowingModel;
+
 	bool Helpers::IsClassicWindow()
 	{
+		// To avoid issues with AppPolicyGetWindowingModel and Store certification, this method cannot be called normally
+		// See https://github.com/microsoft/react-native-windows/pull/5369 (though OneCoreUAP_apiset.lib did pass either)
+
+		if (!s_appPolicyGetWindowingModel)
+		{
+			s_appPolicyGetWindowingModel =
+				reinterpret_cast<AppPolicyGetWindowingModelPtr>(GetProcAddress(GetModuleHandleW(L"Api-ms-win-appmodel-runtime-l1-1-2.dll"), "AppPolicyGetWindowingModel"));
+		}
 		AppPolicyWindowingModel model;
-		return FAILED(AppPolicyGetWindowingModel(GetCurrentThreadEffectiveToken(), &model))
+		return !(s_appPolicyGetWindowingModel && SUCCEEDED(s_appPolicyGetWindowingModel(GetCurrentThreadEffectiveToken(), &model)))
 			|| model != AppPolicyWindowingModel_Universal;
 	}
 }
