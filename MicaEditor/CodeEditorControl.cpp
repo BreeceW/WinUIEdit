3799d2079c1b03271b7eb46beb9e2f69b0831299
Breece Walker <breece@outlook.com>
2023-07-06 01:29:36 -0400

    Set some defaults on CodeEditorControl

--- MicaEditor/CodeEditorControl.cpp
+++ MicaEditor/CodeEditorControl.cpp
@@ -23,6 +23,10 @@ namespace winrt::MicaEditor::implementation
 		DefaultStyleKey(winrt::box_value(L"MicaEditor.CodeEditorControl"));
 
 		_editor = make_self<MicaEditorControl>();
+		AddKeyboardShortcuts();
+		ChangeDefaults();
+		_editor->DpiChanged({ this, &CodeEditorControl::Editor_DpiChanged });
+		_editor->ScintillaNotification({ this, &CodeEditorControl::Editor_ScintillaNotification });
 
 		_gettingFocusRevoker = GettingFocus(auto_revoke, { this, &CodeEditorControl::OnGettingFocus });
 
@@ -87,5 +91,62 @@ namespace winrt::MicaEditor::implementation
 	MicaEditor::Editor CodeEditorControl::Editor()
 	{
 		return _editor->Editor();
+	}
+
+	void CodeEditorControl::Editor_DpiChanged(IInspectable const &sender, double value)
+	{
+		_dpiScale = value;
+		_editor->PublicWndProc(Scintilla::Message::SetXCaretPolicy, CARET_SLOP | CARET_STRICT | CARET_EVEN, ConvertFromDipToPixelUnit(24, value));
+		// Todo: Replace placeholder 1 for caret width from UISettings
+		_editor->PublicWndProc(Scintilla::Message::SetCaretWidth, ConvertFromDipToPixelUnit(1, value), 0); // Todo: Needs to stop blinking after timeout and respect blink rate
+		UpdateZoom();
+	}
+
+	void CodeEditorControl::Editor_ScintillaNotification(IInspectable const &sender, uint64_t value)
+	{
+		const auto data{ reinterpret_cast<Scintilla::NotificationData *>(value) };
+		if (data->nmhdr.code == Scintilla::Notification::Zoom
+			|| (data->nmhdr.code == Scintilla::Notification::Modified && data->linesAdded))
+		{
+			UpdateZoom();
+		}
+	}
+
+	void CodeEditorControl::UpdateZoom()
+	{
+		const auto size{ _editor->PublicWndProc(Scintilla::Message::StyleGetSizeFractional, static_cast<Scintilla::uptr_t>(Scintilla::StylesCommon::Default), 0) };
+		const auto zoom{ static_cast<int>(_editor->PublicWndProc(Scintilla::Message::GetZoom, 0, 0)) };
+		const auto factor{ static_cast<float>((size + zoom * 100)) / size };
+		// Todo: Width 11 hard coded for a digit. Use below for real value:
+		// _editor->PublicWndProc(Scintilla::Message::TextWidth, static_cast<Scintilla::uptr_t>(Scintilla::StylesCommon::LineNumber), reinterpret_cast<Scintilla::sptr_t>("9"))
+		const auto line{ _editor->PublicWndProc(Scintilla::Message::GetLineCount, 0, 0) };
+		const auto width{ 12 + 11 * std::max(3, static_cast<int>(std::floor(std::log10(line) + 1))) };
+		_editor->PublicWndProc(Scintilla::Message::SetMarginWidthN, 0, ConvertFromDipToPixelUnit(std::floorf(factor * width + 0.5f), _dpiScale));
+		_editor->PublicWndProc(Scintilla::Message::SetMarginLeft, 0, ConvertFromDipToPixelUnit(std::floorf(factor * 23 + 0.5f), _dpiScale));
+	}
+
+	void CodeEditorControl::AddKeyboardShortcuts()
+	{
+		_editor->PublicWndProc(Scintilla::Message::AssignCmdKey, 187 + (SCMOD_CTRL << 16), SCI_ZOOMIN); // Ctrl+Plus
+		_editor->PublicWndProc(Scintilla::Message::AssignCmdKey, 189 + (SCMOD_CTRL << 16), SCI_ZOOMOUT); // Ctrl+Minus
+		_editor->PublicWndProc(Scintilla::Message::AssignCmdKey, 48 + (SCMOD_CTRL << 16), SCI_SETZOOM); // Ctrl+0
+	}
+
+	void CodeEditorControl::ChangeDefaults()
+	{
+		_editor->PublicWndProc(Scintilla::Message::SetMultipleSelection, true, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetScrollWidth, 2000, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetScrollWidthTracking, true, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetYCaretPolicy, CARET_SLOP | CARET_STRICT | CARET_EVEN, 1);
+		_editor->PublicWndProc(Scintilla::Message::SetVisiblePolicy, VISIBLE_SLOP, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetHScrollBar, true, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetEndAtLastLine, false, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetTabWidth, 4, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetMarginWidthN, 1, 0);
+		_editor->PublicWndProc(Scintilla::Message::StyleSetFont, STYLE_DEFAULT, reinterpret_cast<Scintilla::sptr_t>("Cascadia Code"));
+		_editor->PublicWndProc(Scintilla::Message::StyleSetSizeFractional, STYLE_DEFAULT, 11 * SC_FONT_SIZE_MULTIPLIER);
+		_editor->PublicWndProc(Scintilla::Message::SetAdditionalSelectionTyping, 1, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetMultiPaste, SC_MULTIPASTE_EACH, 0);
+		_editor->PublicWndProc(Scintilla::Message::SetLayoutThreads, 16, 0); // Todo: Determine performance impact
 	}
 }
--- MicaEditor/CodeEditorControl.h
+++ MicaEditor/CodeEditorControl.h
@@ -9,6 +9,10 @@ namespace winrt::MicaEditor::implementation
 	struct CodeEditorControl : CodeEditorControlT<CodeEditorControl>
 	{
 		CodeEditorControl();
+		~CodeEditorControl()
+		{
+			to_hstring(L"");
+		}
 		uint64_t Scintilla(ScintillaMessage const &message, uint64_t wParam, uint64_t lParam);
 		MicaEditor::Editor Editor();
 		void OnApplyTemplate();
@@ -22,6 +26,12 @@ namespace winrt::MicaEditor::implementation
 		bool _hasFcu{ Windows::Foundation::Metadata::ApiInformation::IsApiContractPresent(L"Windows.Foundation.UniversalApiContract", 5) }; // Todo: Make static
 		bool _has1803{ Windows::Foundation::Metadata::ApiInformation::IsApiContractPresent(L"Windows.Foundation.UniversalApiContract", 6) }; // Todo: Make static
 #endif
+		double _dpiScale;
+		void Editor_DpiChanged(Windows::Foundation::IInspectable const &sender, double value);
+		void Editor_ScintillaNotification(Windows::Foundation::IInspectable const &sender, uint64_t value);
+		void UpdateZoom();
+		void AddKeyboardShortcuts();
+		void ChangeDefaults();
 	};
 }
 
