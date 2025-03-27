78b30ae4a5bed36a0daefd3f78c5f62cf30fcf9e
CaptainWelpe <michael.keller@minesworld.de>
2025-03-28 00:46:46 +0100

    - check against S_OK before drawing - restrict complete redraw to visible area

--- scintilla/winui/ScintillaWin.cxx
+++ scintilla/winui/ScintillaWin.cxx
@@ -2614,7 +2614,7 @@ namespace Scintilla::Internal {
 			paintState = PaintState::painting;
 			paintingAllText = true;
 			// Todo: This is in contradiction with the above paragraph about drawing size. But invalidation results in flickering
-			DrawBit(RECT{ 0, 0, _mainWrapper->Width(), _mainWrapper->Height() });
+			DrawBit(visibleArea); // draw ony visible area, _mainWrapper->Width() & Height() can be too big
 		}
 
 		paintState = PaintState::notPainting;
@@ -2651,7 +2651,7 @@ namespace Scintilla::Internal {
 			_mainWrapper->CreateGraphicsDevices();
 			InvalidateStyleRedraw(); // just Redraw() does not work
 		}
-		else
+		else if (beginDrawHR == S_OK)
 		{
 			const auto &d2dDeviceContext{ _mainWrapper->D2dDeviceContext() };
 
@@ -3201,5 +3201,10 @@ namespace Scintilla::Internal {
 		}
 
 		sisNativeWithD2D->EndDraw();
+	}
+
+	void ScintillaWinUI::SetVisibleArea(LONG x, LONG y, LONG width, LONG height)
+	{
+		visibleArea = RECT{ x, y, width, height };
 	}
 }
